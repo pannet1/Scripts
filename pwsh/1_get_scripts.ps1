fb50cfd75615aa7a448e3a6cf76b7600f6f0b529
b karthick <prog@ecomsense.in>
2025-10-25 05:01:30 +0530

    add pwsh scripts

---
+++ pwsh/1_get_scripts.ps1
@@ -0,0 +1,4 @@
+$Url = "https://github.com/<OWNER>/<REPOSITORY>/archive/<BRANCH_OR_TAG>.zip"
+$OutputZip = "C:\Path\To\Save\Repo.zip"
+
+curl.exe -L -o $OutputZip $Url
---
+++ pwsh/3_install_debian.ps1
@@ -0,0 +1,7 @@
+# COMMAND START: Part 2B - Install Debian
+
+# 1. Install Debian from the Microsoft Store (using the wsl command line tool)
+Write-Host "Installing Debian... This may take a few minutes."
+wsl --install -d Debian
+
+# COMMAND END: Part 2B
