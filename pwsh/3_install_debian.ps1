a89b5e37b87655d8f9d9d1f1c3afca84e3fd5423
b karthick <prog@ecomsense.in>
2025-10-25 07:15:20 +0530

    fix git repo url

--- pwsh/3_install_debian.ps1
+++ pwsh/3_install_debian.ps1
@@ -1,9 +1,7 @@
+winget install -e --id Microsoft.WindowsStore --source msstore
 # COMMAND START: Part 2B - Install Debian
-
 # 1. Install Debian from the Microsoft Store (using the wsl command line tool)
 Write-Host "Installing Debian... This may take a few minutes."
 wsl --install -d Debian
-
-winget install -e --id Microsoft.WindowsStore --source msstore
 
 # COMMAND END: Part 2B
