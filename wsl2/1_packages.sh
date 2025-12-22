673be6cc4363e03db0efaf92f248484f22fddf69
pannet1 <prog@ecomsense.in>
2025-12-23 00:53:32 +0530

    add zoxide

--- wsl2/1_packages.sh
+++ wsl2/1_packages.sh
@@ -1,2 +1,3 @@
-apt install -y git curl wget fontconfig file tar zip unzip gzip tmux xclip
-apt update -y && apt upgrade -y
+sudo ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
+sudo apt install -y git curl wget fontconfig file tar zip unzip gzip tmux xclip
+sudo apt update -y && sudo apt upgrade -y
