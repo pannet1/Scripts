ee283ede127a55c6a0292b11a48d6a9f1f810983
pannet1 <prog@ecomsense.in>
2025-12-23 00:54:41 +0530

    add starship bashrc

--- wsl2/6_starship.sh
+++ wsl2/6_starship.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+
 curl -sS https://starship.rs/install.sh | sh
 sudo apt install console-setup dpkg-reconfigure
 sudo dpkg-reconfigure console-setup
+
+# We use a backslash \" to tell the script: "Put a literal quote here"
+LINE="eval \"\$(starship init bash)\""
+
+if grep -Fxq "$LINE" ~/.bashrc; then
+  echo "Already exists."
+else
+  echo "$LINE" >>~/.bashrc
+  echo "Added successfully!"
+fi
