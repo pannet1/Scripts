7b3dd37683a985098153d35d78e36c3c82bb8b0b
pannet1 <prog@ecomsense.in>
2025-12-23 02:55:37 +0530

    move date to init

--- wsl2/6_starship.sh
+++ wsl2/6_starship.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 curl -sS https://starship.rs/install.sh | sh
-sudo apt install console-setup dpkg-reconfigure
-sudo dpkg-reconfigure console-setup
 
 # We use a backslash \" to tell the script: "Put a literal quote here"
 LINE="eval \"\$(starship init bash)\""
