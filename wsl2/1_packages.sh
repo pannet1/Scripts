c23feb0d9b0dfa15e691ea61f733e4dd53826c15
pannet1 <prog@ecomsense.in>
2026-02-15 02:52:18 +0530

    no need to install reconfigure

--- wsl2/1_packages.sh
+++ wsl2/1_packages.sh
@@ -1,6 +1,6 @@
-sudo apt install console-setup dpkg-reconfigure -y
+sudo apt update -y && sudo apt upgrade -y
+sudo apt install console-setup -y
 sudo dpkg-reconfigure console-setup
 
 sudo ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
 sudo apt install -y git curl wget fontconfig file tar zip unzip gzip tmux xclip
-sudo apt update -y && sudo apt upgrade -y
