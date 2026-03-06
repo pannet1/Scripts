83626d90cb65a4e3b1dccb6d6c2983d33afa6ee4
pannet1 <prog@ecomsense.in>
2026-03-06 08:18:05 +0530

    fix test bug

--- alphine/connect_wifi.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "Scanning for WiFi..."
-nmcli device wifi list
-echo "--------------------------------------"
-echo "To connect, type: nmcli dev wifi connect 'SSID_NAME' password 'PASSWORD'"
