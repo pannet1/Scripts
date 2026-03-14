4ce3952240da17ca75f0600a29d4c465e47abfe5
pannet1 <prog@ecomsense.in>
2026-03-15 03:36:10 +0530

    add dmidecode

--- alpine/install_git.sh
+++ alpine/install_git.sh
@@ -3,8 +3,11 @@ apk add git openssh-client-common
 git config --global user.email "prog@ecomsense.in"
 git config --global user.name "b karthick"
 git config --global url."git@github.com:".insteadOf "https://github.com/"
+git config --global list
 
 mkdir -p ~/.ssh
 ln /mount/usb/id_ed25519.pub ~/.ssh/id_ed25519.pub
+ls ~/.ssh
 
-echo "add Scripts to path in ~/.bashrc"
+echo "add Scripts to path in ~/.profile"
+cat ~/.profile
