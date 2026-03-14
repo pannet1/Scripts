a1aececa9f7ab8dc68352f292160a1c699e8700c
pannet1 <prog@ecomsense.in>
2026-03-14 20:58:58 +0530

    make names verb first

--- alphine/install_nm.sh
+++ alphine/install_nm.sh
@@ -4,13 +4,13 @@ set -e
 echo "--- INSTALLING NETWORK MANAGER & WIFI FIRMWARE ---"
 
 # 1. Update repos
-setup-apkrepos -c
 apk update
 
 # 2. Wireless Radio Blobs
 # iwlwifi = Intel WiFi
 # rtlwifi = Realtek WiFi (Common in Acer)
 # ath10k/11k = Qualcomm/Atheros (Common in Acer/HP)
+echo "installing linux firmware .."
 apk add linux-firmware-iwlwifi \
     linux-firmware-rtlwifi \
     linux-firmware-ath10k \
@@ -19,6 +19,7 @@ apk add linux-firmware-iwlwifi \
 # 3. Install NM, the CLI tool, and universal WiFi firmware
 # Added networkmanager-cli so the command 'nmcli' exists
 # Added linux-firmware-iwlwifi & rtlwifi for Intel/Realtek chips
+echo "installing network manager "
 apk add networkmanager \
     networkmanager-cli \
     networkmanager-wifi \
@@ -31,7 +32,5 @@ touch /media/usb/.boot_repository
 lbu commit -d
 
 echo "------------------------------------------------"
-echo "DONE: 'nmcli' is now available."
 echo "If WiFi is still off, run: rfkill unblock all"
-echo "Then scan with: nmcli dev wifi list"
 echo "------------------------------------------------"
