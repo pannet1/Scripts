e4ef74ac9ca9718deb279b354f4563e601d8d361
pannet1 <prog@ecomsense.in>
2026-03-15 03:36:10 +0530

    add dmidecode

--- alpine/install_test.sh
+++ alpine/install_test.sh
@@ -13,7 +13,7 @@ setup-apkcache /media/usb/cache
 
 # 3. Install the "Technician's Heavy Duty" Package List
 # kbd = keyboard test, alsa-utils = audio, pciutils = lspci
-apk add smartmontools memtester stress-ng acpi pciutils nvme-cli kbd alsa-utils
+apk add smartmontools memtester stress-ng acpi pciutils nvme-cli kbd alsa-utils dmidecode
 apk add alsa-utils alsa-ucm-conf
 rc-service alsa start
 # 4. Sync and Persist
