637820c75e5ea7da50ea72b3c4f43abfc76b0cee
pannet1 <prog@ecomsense.in>
2026-03-15 01:12:45 +0530

    add profile

--- alpine/install_git.sh
+++ alpine/install_git.sh
@@ -2,6 +2,7 @@ apk update
 apk add git ssh-client
 git config --global user.email "prog@ecomsense.in"
 git config --global user.name "b karthick"
+git config --global url."git@github.com:".insteadOf "https://github.com/"
 
 mkdir -p ~/.ssh
 ln /mount/usb/id_ed25519.pub ~/.ssh/id_ed25519.pub
