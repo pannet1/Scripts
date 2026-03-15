c930d52fd3ba4848c19caa472113a4b4a97b8513
pannet1 <prog@ecomsense.in>
2026-03-15 05:15:58 +0000

    fix alphine usb path

--- alpine/enable_git.sh
+++ alpine/enable_git.sh
@@ -1,6 +1,6 @@
 mkdir -p ~/.ssh
-cp /mount/usb/id_ed25519.pub ~/.ssh/id_ed25519.pub
-cp /mount/usb/id_ed25519 ~/.ssh/id_ed25519
+cp /media/usb/id_ed25519.pub ~/.ssh/id_ed25519.pub
+cp /media/usb/id_ed25519 ~/.ssh/id_ed25519
 ls -la ~/.ssh
 
 ssh -V
