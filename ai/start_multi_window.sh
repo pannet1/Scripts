7895836f03fb038e131a67a225260c204cf1bd38
pannet1 <prog@ecomsense.in>
2026-04-13 15:34:13 +0530

    add server script

--- ai/start_multi_window.sh
+++ ai/start_multi_window.sh
@@ -1,4 +1,4 @@
 #/bin/env sh
 
 # worked before installing nvidia
-./Qwen3.5-0.8B-Q8_0.llamafile --server --host 0.0.0.0 --jinja --ctx-size 64000
+~/Qwen3.5-0.8B-Q8_0.llamafile --server --host 0.0.0.0 --jinja --ctx-size 64000
