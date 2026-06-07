af5f07bcd6085c32aacf5cfab5232bff3e3bdcc2
pannet1 <12350276+pannet1@users.noreply.github.com>
2026-06-08 01:51:46 +0530

    fix: skip DSR query for faster startup in WSL2

--- common/.config/nvim/lua/config/options.lua
+++ common/.config/nvim/lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Skip DSR query on startup for terminals that don't respond (WSL2/Windows Terminal)
+vim.opt.guicursor = ""
