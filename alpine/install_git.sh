2424c39d52e25c1280307f01dae407c110e141b9
pannet1 <12350276+pannet1@users.noreply.github.com>
2026-07-01 10:48:58 +0530

    alpine: switch remote to HTTPS, drop SSH key setup from install_git.sh

--- alpine/install_git.sh
+++ alpine/install_git.sh
@@ -3,15 +3,8 @@ apk update
 apk add git openssh-client-default
 git config --global user.email "prog@ecomsense.in"
 git config --global user.name "b karthick"
-git config --global url."git@github.com:".insteadOf "https://github.com/"
-git config --global list
-
-mkdir -p ~/.ssh
-cp /mount/usb/id_ed25519.pub ~/.ssh/id_ed25519.pub
-cp /mount/usb/id_ed25519 ~/.ssh/id_ed25519
-ls -la ~/.ssh
-
-ssh -V
+git config --global credential.helper store
+git config --list
 echo "add Scripts to path in ~/.profile"
 cat ~/.profile
 
